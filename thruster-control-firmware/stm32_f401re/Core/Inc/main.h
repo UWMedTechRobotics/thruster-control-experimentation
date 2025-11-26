5e817e146d5968ebb52046888af14264ba592d5b
Adrian Tarantino <adriantarantino2006@gmail.com>
2025-11-25 21:33:15 -0500

    Merging with firmware-setup

--- thruster-control-firmware/stm32_f401re/Core/Inc/main.h
+++ thruster-control-firmware/stm32_f401re/Core/Inc/main.h
@@ -27,7 +27,7 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "./../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h"
+#include "stm32f4xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
